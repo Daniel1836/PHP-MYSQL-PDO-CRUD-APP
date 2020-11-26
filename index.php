46346729d563c3dcf96521d8e50c8e3ce70a73ef
Daniel Klein <daniel.klein@videotron.ca>
2020-11-26 11:11:04 -0500

    Update and rename Assignment4PDO.php to index.php

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-            <link rel="stylesheet" href="crudcss.css">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">  
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-            <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
-   <title>CRUD App</title>
-        </head>
-   <body> 
-
  <?php 
- require 'ProcessPDO.php';
+ require 'Process.php';
  ?>
 
  <?php if (isset($_SESSION['message'])): ?>
@@ -30,7 +15,7 @@
 
 <?php endif ?>
      
-       <form action="ProcessPDO.php" method="POST">
+       <form action="Process.php" method="POST">
        <input type="hidden" name="id" value="<?php echo $id;?>">
        <label>ID</label>
            <input type="text" name="id" value="<?php echo $id;?>" placeholder="Enter ID">
@@ -70,7 +55,3 @@
 </tr>
   
     <?php endforeach; ?>
-
-
-   </body>
-</html>
