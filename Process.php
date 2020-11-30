3eb5b22ab083a94952a2007b4761ecb20e8b3c0a
Daniel Klein <daniel.klein@videotron.ca>
2020-11-30 07:57:52 -0500

    Update Process.php

--- Process.php
+++ Process.php
@@ -10,9 +10,9 @@ foreach($_POST as $key=>$value)
 //Insert
 if (isset($_POST['save']))
 {
-$sql='insert into crud3(id, name, location)values(?, ?, ?);';
+$sql='insert into crud4(name, location)values(?, ?);';
 $stmt=$pdo->prepare($sql);
-$stmt->execute(array($id, $name, $location));
+$stmt->execute(array($name, $location));
 header("location: Assignment4PDO.php");
 }
 
@@ -20,14 +20,14 @@ header("location: Assignment4PDO.php");
 if (isset($_GET['delete']))
 {
 $id = $_GET['delete'];
-$sql = 'DElETE FROM crud3 WHERE id = ?';
+$sql = 'DElETE FROM crud4 WHERE id = ?';
 $stmt=$pdo->prepare($sql);
 $stmt->execute([$id]);
 header("location: Assignment4PDO.php");
 }
 
 //Read
-$sql = 'select * from crud3';
+$sql = 'select * from crud4';
 $stmt=$pdo->prepare($sql);
 $stmt->execute();
 $info = $stmt->fetchAll();
@@ -37,7 +37,7 @@ if (isset($_GET['edit']))
 {
     $id = $_GET['edit'];
     $update = true;
-    $sql = 'select * from crud3 WHERE id=?';
+    $sql = 'select * from crud4 WHERE id=?';
     $stmt=$pdo->prepare($sql);
     $stmt->execute([$id]);
     $info = $stmt->fetchAll();
@@ -45,9 +45,9 @@ if (isset($_GET['edit']))
     if (count($info==1))
     {
       foreach($info as $data):
-      	$name = $data[2];
+      	$name = $data[1];
         $id= $data[0];
-        $location = $data[1];
+        $location = $data[2];
         endforeach;
     }
 }
@@ -55,13 +55,15 @@ if (isset($_GET['edit']))
 //Update
  if (isset($_POST['update']))
  {
-        $id = $_POST['id'];
+         $id = $_POST['id'];
         $name = $_POST['name'];
         $location = $_POST['location'];
-        $sql= 'UPDATE crud3 SET id=:id, name=:name,location=:location WHERE id=:id';
+         // $sql= "UPDATE crud4 SET name=?, location=? WHERE id=?";
+        //$stmt=$pdo->prepare($sql);
+   //$stmt->execute([$name, $location]);
+       $sql= 'UPDATE crud4 SET name=:name,location=:location WHERE id=:id';
         $stmt=$pdo->prepare($sql);
-        $stmt->execute([':id'=> $id, ':name'=> $name,':location'=> $location]);
+        $stmt->execute([':id'=> $id,':name'=> $name,':location'=> $location]);
         header("location: Assignment4PDO.php");     
  }
-
 ?>
